355fea4d7ed687a03217f7d225215dff4e00c424
hasanuro <148393835+hasanuro@users.noreply.github.com>
2024-05-30 00:24:48 +0500

    Add files via upload

---
+++ JSSession1.js
@@ -0,0 +1,49 @@
+alert("javascript first class");
+document.write("Urooj <br>");
+document.write("Hasan <br>");
+document.write("Ahmed <br>");
+document.write("Sara <br>");
+document.write(200 + "<br>");
+document.write(250 + 750 , "<br>" );
+document.write("Kainat Sahani <br>");
+console.log("JS Session 1");
+
+//VARIABLE :USED TO STORE SOMETHING
+//THREE TYPES OF VAR
+//1-VAR 
+// var fname; //declare
+// fname = "ali khan"; //init
+
+//write way to make variable
+//fname 
+//fName
+//f_name
+//fname1
+ 
+//wrong way to make variable
+//f-name
+//f name
+//2fname
+
+var fname="Urooj Hasan"; //declare and init
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+document.write(fname);
+// document.write(fname);
+// document.write(fname);
+// document.write("ali khan");
+// document.write("ali");
+// document.write("ali");
+// document.write("ali");
+// document.write("ali");
+// document.write("ali");
+// document.write("ali");
+// document.write("ali");
+// document.write("ali");
